7a7b5a60d79b13557dab1e4349d68a4fa0422885
gregorio-corchado-02 <gregorio02cg@gmail.com>
2024-05-29 23:24:26 -0600

    Practica No.4 JS: Asincronía con JS

--- JS4React/index.js
+++ JS4React/index.js
@@ -30,7 +30,7 @@ const usuario={
     correo: eviarcor=()=>'enviando'
 };
 
-console.log(usuario.correo())*/
+console.log(usuario.correo())
 
 const titulo = document.createElement('h1')
 titulo.innerText='Soy un Titulo'
@@ -44,4 +44,47 @@ boton.addEventListener('click',function(){
     console.log('Evento click ejecutado')
     titulo.innerText="Evento click ejecutado"
     alert('Exito')
-})
+})*/
+
+const ul = document.createElement('ul');
+
+let datos = fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(function(response){
+                console.log('Carga de datos completada');
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data);
+                data.forEach(function(post){
+                    const li = document.createElement('li');
+                    li.innerHTML = post.title;
+                    ul.append(li);
+                });
+                document.body.append(ul);       
+            })
+
+
+console.log('Cargando HTML')
+console.log('Cargando CSS')
+console.log('Cargando Imagenes')
+
+console.log(datos);
+
+/*const ul = document.createElement('ul');
+
+async function cargarDatos(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const datos = await response.json()
+    console.log(datos)
+    datos.forEach(function(post){
+        const li = document.createElement('li');
+        li.innerHTML = post.title;
+        ul.append(li);
+    });
+    document.body.append(ul); 
+}
+
+cargarDatos()
+console.log('Cargando HTML')
+console.log('Cargando CSS')
+console.log('Cargando Imagenes')*/
