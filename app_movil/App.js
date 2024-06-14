aacb73674ad87775e4c2fd77880f8a999c8cbb05
gregorio-corchado-02 <gregorio02cg@gmail.com>
2024-06-14 15:43:24 -0600

    Practica No.7: StyleSheets & Flex

--- app_movil/App.js
+++ app_movil/App.js
@@ -1,24 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-const Texto=()=>{
-  const [contenido, setContenido]= useState('hola mundoo')
-  const actulizarContenido=()=>{setContenido('State actualizo este texto')
+const Texto = ({ styles }) => {
+  const [contenido, setContenido] = useState('hola mundoo');
+  const actualizarContenido = () => {
+    setContenido('State actualizo este texto');
+  };
 
-  }
-  return (<Text onPress={actulizarContenido}> {contenido} </Text>)
-
+  return (
+    <Text style={[styles.Text, styles]} onPress={actualizarContenido}>
+      {contenido}
+    </Text>
+  );
 };
 
 export default function App() {
   return (
-
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
-      <Texto contenido={'GREGORIO'}> </Texto>
-      <StatusBar style="auto"/>
-      <Button title='Presiona' color='#000000' ></Button>
+      <Text style={styles.red}>Hola Mundo</Text>
+      <Text style={styles.blue}>Hola Mundo</Text>
+      <Text style={styles.green}>Hola Mundo</Text>
+      <Texto styles={styles.Text} />
+      <StatusBar style="auto" />
+      <Button title="Presiona" color="#000000" />
     </View>
   );
 }
@@ -26,8 +31,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+
+  Text: {
+    color: 'yellow',
+    fontSize: 25,
+    height: 150,
+  },
+
+  red: {
+    backgroundColor: 'red',
+  },
+
+  blue: {
+    backgroundColor: 'blue',
+  },
+
+  green: {
+    backgroundColor: 'green',
   },
 });
