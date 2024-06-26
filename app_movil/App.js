4fee79bfb176d64da47a7b81ddba02d3bd5fc62c
gregorio-corchado-02 <gregorio02cg@gmail.com>
2024-06-26 14:58:32 -0600

    SectionList

--- app_movil/App.js
+++ app_movil/App.js
@@ -1,24 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
-
-
+import { StyleSheet, Text, View, FlatList, SectionList} from 'react-native';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={[
+
+    <SectionList style={styles.section}
+      sections={[
+        {title:'Grupo A',
+        data:[
           {key:'1', name:'Gregorio'},
           {key:'2', name:'Gadiel'},
           {key:'3', name:'Antonio'},
           {key:'4', name:'Kevin'},
-          {key:'5', name:'Ricarda'},
+          {key:'5', name:'Ricarda'},],
+        },
 
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
-      />
+        {title:'Grupo B',
+          data:[
+            {key:'6', name:'Marta'},
+            {key:'7', name:'Ana'},
+            {key:'8', name:'Maria'},
+            {key:'9', name:'Luis'},
+            {key:'10', name:'Pedro'},],
+        },
+
+        {title:'Grupo C',
+          data:[
+            {key:'11', name:'Carlos'},
+            {key:'12', name:'Angel'},
+            {key:'13', name:'Daniel'},
+            {key:'14', name:'Roberto'},
+            {key:'15', name:'Rodrigo'},],
+        },
+      ]}
+
+      renderItem={({item})=><Text style={styles.item}>{item.name}</Text>}
+      renderSectionHeader={({section}) => <Text style={styles.section}>{section.title}</Text>}
+    />
+      
     </View>
   );
 }
@@ -37,8 +59,14 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 24,
     height: 50,
-    borderColor:'Red',
+    borderBlockEndColor:'Red',
     borderBottomWidth:1,
+  },
+
+  section:{
+    fontSize:16,
+    fontWeight:'bold',
+    backgroundColor:'#eee',
   },
 
 });
